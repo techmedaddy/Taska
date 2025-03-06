cba060182e64c49f1314cc4ff691c5206aef1880
rajababu <123633963+umarei@users.noreply.github.com>
2025-03-06 18:58:28 +0530

    new routings

--- services/auth-service/src/middleware/validateMiddleware.ts
+++ services/auth-service/src/middleware/validateMiddleware.ts
@@ -12,8 +12,7 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-        return;
+        return res.status(400).json({ errors: errors.array() });
     }
 
     next(); // Proceed to the next middleware or route handler
